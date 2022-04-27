a9d03e9d2478f99cfa72c01065c26398b57b714b
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-04-27 15:46:30 +0200

    creation des packages du MVC

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by etienne on 27/04/2022.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, Carcassonne !" << endl;
+    return 0;
+}
---
+++ modele/zone.h
@@ -0,0 +1,14 @@
+//
+// Created by etienne on 27/04/2022.
+//
+
+#ifndef CARCASSONNE_LO21_P22_ZONE_H
+#define CARCASSONNE_LO21_P22_ZONE_H
+
+
+class zone {
+
+};
+
+
+#endif //CARCASSONNE_LO21_P22_ZONE_H
