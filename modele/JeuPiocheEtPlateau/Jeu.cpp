dfb92b0ef27eaf95816c60504663512ac79c6c03
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-05-08 12:33:25 +0200

    Lire le contenu d'un dossier des ressources

--- modele/JeuPiocheEtPlateau/Jeu.cpp
+++ modele/JeuPiocheEtPlateau/Jeu.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "Jeu.h"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <dirent.h>
+
+typedef std::vector<std::string> stringvec;
+
+void Jeu::getTuilesDesRessources() {
+    stringvec v;
+    lireDossier("../" + TUILES_NORMALES, v);
+
+    for (auto &s : v) {
+        std::cout << s << std::endl;
+
+
+
+
+    }
+}
+
+void Jeu::lireDossier(const std::string& name, stringvec& v)
+{
+    DIR* dirp = opendir(name.c_str());
+    struct dirent * dp;
+    while ((dp = readdir(dirp)) != nullptr) {
+        if (dp->d_name[0] != '.') {
+            v.push_back(dp->d_name);
+        }
+    }
+    closedir(dirp);
+}
--- modele/JeuPiocheEtPlateau/Jeu.h
+++ modele/JeuPiocheEtPlateau/Jeu.h
@@ -4,10 +4,21 @@
 
 #ifndef CARCASSONNE_LO21_P22_JEU_H
 #define CARCASSONNE_LO21_P22_JEU_H
+#include <iostream>
+#include <sys/types.h>
+#include <dirent.h>
+#include <vector>
+using namespace std;
+typedef std::vector<std::string> stringvec;
 
+const string RESSOURCES = "Ressources/";
+const string TUILES_NORMALES = RESSOURCES+"TuilesNormales/";
+const string TUILES_RIVIERES = RESSOURCES+"TuilesRiviere/";
 
 class Jeu {
-
+    static void lireDossier(const std::string& name, stringvec& v);
+public:
+    static void getTuilesDesRessources();
 };
 
 
