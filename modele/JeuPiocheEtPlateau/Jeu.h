da94cc8c64a13a3c1117966ede70315a1aa7e0e5
Etienne Clr <60063320+EtienneClr-Projects@users.noreply.github.com>
2022-05-08 22:54:43 +0200

    méthode getTuileDesRessources

--- modele/JeuPiocheEtPlateau/Jeu.h
+++ modele/JeuPiocheEtPlateau/Jeu.h
@@ -1,13 +1,30 @@
-//
-// Created by etienne on 27/04/2022.
-//
 
 #ifndef CARCASSONNE_LO21_P22_JEU_H
 #define CARCASSONNE_LO21_P22_JEU_H
 
+#include <iostream>
+#include <sys/types.h>
+#include <dirent.h>
+#include <vector>
+#include "CasesTuilesEtZones/Tuile.h"
+
+using namespace std;
+typedef std::vector<std::string> stringVec;
+
+const string RESSOURCES = "Ressources/";
+const string RESSOURCES_NORMALES = RESSOURCES + "TuilesNormales/";
+const string RESSOURCES_RIVIERES = RESSOURCES + "TuilesRiviere/";
+const string RESSOURCES_PAYSANS = RESSOURCES + "TuilesPaysans/";
+const string RESSOURCES_ABBE = RESSOURCES + "TuilesAbbe/";
+const string RESSOURCES_AUBERGES_CATHEDRALES = RESSOURCES + "TuilesAubergesCathedrales/";
 
 class Jeu {
+    static void lireDossier(const std::string &chemin, stringVec &vecteurDeStrings);
 
+public:
+    static map<DIRECTION, Case *> deepCopyMap(const map<DIRECTION, Case *>& map1);
+
+    static void getTuilesDesRessources(EXTENSION extension, vector<Tuile *> *tuiles);
 };
 
 
