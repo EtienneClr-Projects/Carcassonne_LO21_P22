32eb7effd1695292bb8b1d2f692997d7a2d7c19c
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-05-10 14:47:56 +0200

    Classe Coord

---
+++ modele/Gestion/Coord.h
@@ -0,0 +1,35 @@
+//
+// Created by etienne on 10/05/2022.
+//
+
+#ifndef CARCASSONNE_LO21_P22_COORD_H
+#define CARCASSONNE_LO21_P22_COORD_H
+
+
+#include <string>
+
+class Coord {
+    int x_;
+    int y_;
+
+public:
+    Coord(int x, int y) {
+        this->x_ = x;
+        this->y_ = y;
+    }
+
+    int x() const {
+        return x_;
+    }
+
+    int y() const {
+        return y_;
+    }
+
+    std::string toString() const {
+        return "("+std::to_string(x_) + "," + std::to_string(y_)+")";
+    };
+};
+
+
+#endif //CARCASSONNE_LO21_P22_COORD_H
