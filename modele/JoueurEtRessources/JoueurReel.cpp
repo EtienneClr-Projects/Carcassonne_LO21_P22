1d3e0299e9a30a038e8b601471fc4d9155b9fc58
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-05-11 14:35:18 +0200

    Création des classes JoueurVirtuel et Réel

---
+++ modele/JoueurEtRessources/JoueurReel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by etienne on 10/05/2022.
+//
+
+#include "JoueurReel.h"
---
+++ modele/JoueurEtRessources/JoueurVirtuel.h
@@ -0,0 +1,17 @@
+//
+// Created by etienne on 10/05/2022.
+//
+
+#ifndef CARCASSONNE_LO21_P22_JOUEURVIRTUEL_H
+#define CARCASSONNE_LO21_P22_JOUEURVIRTUEL_H
+
+
+#include "Joueur.h"
+
+class JoueurVirtuel: public Joueur {
+    public:
+        JoueurVirtuel();
+};
+
+
+#endif //CARCASSONNE_LO21_P22_JOUEURVIRTUEL_H
