545f4b638e9927d28f493454e702cc342b5bba8a
Silvia <silvia@macbook-pro-de-silvia.home>
2022-05-16 23:46:05 +0200

    Version 1

--- modele/CasesTuilesEtZones/Tuile.cpp
+++ modele/CasesTuilesEtZones/Tuile.cpp
@@ -1,5 +1,52 @@
 //
-// Created by etienne on 27/04/2022.
+//  Tuile.cpp
+//  Carcassonne
+//
+//  Created by Silvia on 11/05/2022.
 //
 
-#include "Tuile.h"
+#include "Case.hpp"
+#include "Tuile.hpp"
+#include <stdio.h>
+# include <iostream>
+# include <string>
+# include <initializer_list>
+# include <cstdlib>
+# include <map>
+
+
+using namespace std;
+using namespace Carcassonne;
+
+namespace Carcassonne {
+
+    Tuile::Tuile(map<DIRECTION,Case*> t):tuile(t) {};
+
+    Case* Tuile::getCase(DIRECTION d )const{
+        return tuile.find(d)->second;
+    }
+
+
+    void Tuile::PivoterTuile( int nbre_rotation){
+        map<DIRECTION,Case*> t2;
+        for (unsigned int i=0; i<nbre_rotation; i++){
+            t2[DIRECTION::NORD_OUEST]=tuile.find( DIRECTION::SUD_OUEST)->second;
+            t2[DIRECTION::NORD]=tuile.find( DIRECTION::OUEST)->second;
+            t2[DIRECTION::NORD_EST]=tuile.find( DIRECTION::NORD_OUEST)->second;
+            t2[DIRECTION::OUEST]=tuile.find( DIRECTION::SUD)->second;
+            t2[DIRECTION::MILIEU]=tuile.find( DIRECTION::MILIEU)->second;
+            t2[DIRECTION::EST]=tuile.find( DIRECTION::NORD)->second;
+            t2[DIRECTION::SUD_OUEST]=tuile.find( DIRECTION::SUD_EST)->second;
+            t2[DIRECTION::SUD]=tuile.find( DIRECTION::EST)->second;
+            t2[DIRECTION::SUD_EST]=tuile.find( DIRECTION::NORD_EST)->second;
+            tuile=t2;
+            /*for(DIRECTION d=DIRECTION::NORD_OUEST; d!=DIRECTION::SUD_EST; d=(DIRECTION) (d+1)) {//si jamais on garde attribut cardinal dans cases
+                tuile.find(d)->second->setDirection(d);
+            }*/
+        }
+    }
+    
+    
+
+}
+
--- modele/CasesTuilesEtZones/Tuile.h
+++ modele/CasesTuilesEtZones/Tuile.h
@@ -1,14 +1,41 @@
 //
-// Created by etienne on 27/04/2022.
+//  Tuile.hpp
+//  Carcassonne
+//
+//  Created by Silvia on 11/05/2022.
 //
 
-#ifndef CARCASSONNE_LO21_P22_TUILE_H
-#define CARCASSONNE_LO21_P22_TUILE_H
+#ifndef Tuile_hpp
+#define Tuile_hpp
+
+#include "Case.hpp"
+#include <stdio.h>
+# include <iostream>
+# include <string>
+# include <initializer_list>
+# include <cstdlib>
+# include <map>
 
 
+using namespace std;
+
+namespace Carcassonne {
 class Tuile {
+    public:
+        map<DIRECTION,Case *> getTuile()const{return tuile;};
+        Case* getCase(DIRECTION d)const;
+        Tuile(map<DIRECTION,Case*> m);
+        void PivoterTuile(int nbre_rotation);
+        // explicitation des opérations générées par défaut
+        ~Tuile() = default;
+        Tuile(const Tuile&) = default;
+        Tuile& operator=(const Tuile&) = default;
+    private:
+        map<DIRECTION,Case*> tuile;
+        
+    };
 
-};
 
 
-#endif //CARCASSONNE_LO21_P22_TUILE_H
+}
+#endif /* Tuile_hpp */
