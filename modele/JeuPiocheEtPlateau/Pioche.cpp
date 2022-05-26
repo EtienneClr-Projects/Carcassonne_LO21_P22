5259d6cf9508abd2b1850bf5b89c6ad8c87b7dd1
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-05-26 22:25:54 +0200

    Création de Pioche

--- modele/JeuPiocheEtPlateau/Pioche.cpp
+++ modele/JeuPiocheEtPlateau/Pioche.cpp
@@ -3,3 +3,21 @@
 //
 
 #include "Pioche.h"
+
+Pioche::Pioche(std::vector<Tuile *> tuiles) {
+    this->tuiles = tuiles;
+    this->nbTuilesRestantes = tuiles.size();
+}
+
+Tuile *Pioche::piocher() {
+    //renvoie une tuile au hasard
+    if (nbTuilesRestantes == 0) {
+        return nullptr;
+    }
+    int index = rand() % nbTuilesRestantes;
+    Tuile *tuile = tuiles[index];
+    tuiles.erase(tuiles.begin() + index);
+    nbTuilesRestantes--;
+    return tuile;
+}
+
--- modele/JeuPiocheEtPlateau/Pioche.h
+++ modele/JeuPiocheEtPlateau/Pioche.h
@@ -4,9 +4,18 @@
 
 #ifndef CARCASSONNE_LO21_P22_PIOCHE_H
 #define CARCASSONNE_LO21_P22_PIOCHE_H
+#include <iostream>
+#include "CasesTuilesEtZones/Tuile.h"
 
+using namespace std;
 
 class Pioche {
+private:
+    std::vector<Tuile *> tuiles;
+    int nbTuilesRestantes;
+public:
+    Pioche(std::vector<Tuile*> tuiles);
+    Tuile* piocher();
 
 };
 
