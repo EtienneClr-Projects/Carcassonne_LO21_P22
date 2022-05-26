669f4a87edf20a9beeeb3a313e65ec20c9ef6b07
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-26 21:25:57 +0200

    Update Tuile.cpp

--- modele/CasesTuilesEtZones/Tuile.cpp
+++ modele/CasesTuilesEtZones/Tuile.cpp
@@ -1,12 +1,6 @@
-//
-//  Tuile.cpp
-//  Carcassonne
-//
-//  Created by Silvia on 11/05/2022.
-//
 
-#include "Case.hpp"
-#include "Tuile.hpp"
+#include "CasesTuilesEtZones/Tuile.h"
+
 #include <stdio.h>
 # include <iostream>
 # include <string>
@@ -16,37 +10,41 @@
 
 
 using namespace std;
-using namespace Carcassonne;
 
-namespace Carcassonne {
 
-    Tuile::Tuile(map<DIRECTION,Case*> t):tuile(t) {};
+
+Tuile::Tuile(map<DIRECTION, Case *> cases, std::string cheminImage) {
+    this->cases = std::move(cases);
+    this->cheminImage = std::move(cheminImage);
+}
 
     Case* Tuile::getCase(DIRECTION d )const{
-        return tuile.find(d)->second;
+        return cases.find(d)->second;
     }
-
 
     void Tuile::PivoterTuile( int nbre_rotation){
-        map<DIRECTION,Case*> t2;
-        for (unsigned int i=0; i<nbre_rotation; i++){
-            t2[DIRECTION::NORD_OUEST]=tuile.find( DIRECTION::SUD_OUEST)->second;
-            t2[DIRECTION::NORD]=tuile.find( DIRECTION::OUEST)->second;
-            t2[DIRECTION::NORD_EST]=tuile.find( DIRECTION::NORD_OUEST)->second;
-            t2[DIRECTION::OUEST]=tuile.find( DIRECTION::SUD)->second;
-            t2[DIRECTION::MILIEU]=tuile.find( DIRECTION::MILIEU)->second;
-            t2[DIRECTION::EST]=tuile.find( DIRECTION::NORD)->second;
-            t2[DIRECTION::SUD_OUEST]=tuile.find( DIRECTION::SUD_EST)->second;
-            t2[DIRECTION::SUD]=tuile.find( DIRECTION::EST)->second;
-            t2[DIRECTION::SUD_EST]=tuile.find( DIRECTION::NORD_EST)->second;
-            tuile=t2;
-            /*for(DIRECTION d=DIRECTION::NORD_OUEST; d!=DIRECTION::SUD_EST; d=(DIRECTION) (d+1)) {//si jamais on garde attribut cardinal dans cases
-                tuile.find(d)->second->setDirection(d);
-            }*/
+            map<DIRECTION,Case*> t2;
+            for (unsigned int i=0; i<nbre_rotation%4; i++) {//on decale les cases de 90 degres sans des aiguilles d'une montre
+                t2[DIRECTION::NORD_OUEST] = cases.find(DIRECTION::SUD_OUEST)->second;
+                t2[DIRECTION::NORD] = cases.find(DIRECTION::OUEST)->second;
+                t2[DIRECTION::NORD_EST] = cases.find(DIRECTION::NORD_OUEST)->second;
+                t2[DIRECTION::OUEST] = cases.find(DIRECTION::SUD)->second;
+                t2[DIRECTION::MILIEU] = cases.find(DIRECTION::MILIEU)->second;
+                t2[DIRECTION::EST] = cases.find(DIRECTION::NORD)->second;
+                t2[DIRECTION::SUD_OUEST] = cases.find(DIRECTION::SUD_EST)->second;
+                t2[DIRECTION::SUD] = cases.find(DIRECTION::EST)->second;
+                t2[DIRECTION::SUD_EST] = cases.find(DIRECTION::NORD_EST)->second;
+                cases = t2;
+            }//on modifie egalement la direction à l'intérieur des cases
+            t2[DIRECTION::NORD_OUEST]->setDirection(DIRECTION::NORD_OUEST);
+            t2[DIRECTION::NORD]->setDirection(DIRECTION::NORD);
+            t2[DIRECTION::NORD_EST]->setDirection(DIRECTION::NORD_EST);
+            t2[DIRECTION::OUEST]->setDirection(DIRECTION::OUEST);
+            t2[DIRECTION::MILIEU]->setDirection(DIRECTION::MILIEU);
+            t2[DIRECTION::EST]->setDirection(DIRECTION::EST);
+            t2[DIRECTION::SUD_OUEST]->setDirection(DIRECTION::SUD_OUEST);
+            t2[DIRECTION::SUD]->setDirection(DIRECTION::SUD);
+            t2[DIRECTION::SUD_EST]->setDirection(DIRECTION::SUD_EST);
+            cases=t2;
         }
-    }
-    
-    
-
-}
 
