1479dd92a85e27347409bd97b1c40b1c9cd11044
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-27 19:21:20 +0200

    Update Case.cpp

--- modele/CasesTuilesEtZones/Case.cpp
+++ modele/CasesTuilesEtZones/Case.cpp
@@ -6,17 +6,10 @@
 //
 
 #include "Case.h"
-#include "ParametrePartie.hpp"
-#include <stdio.h>
-# include <iostream>
-# include <string>
-# include <initializer_list>
 
 
-Case::Case(ZONE_TYPE zt, DIRECTION d,SUPP_TYPE s,int i): zone_type(zt),direction(d),supp_type(s),id_connexion(i),tuile_parente(nullptr),zone_parente(nullptr), meeple_posé(nullptr) {};
+Case::Case(ZONE_TYPE zt, DIRECTION d,SUPP_TYPE s,int i): zone_type(zt),direction(d),supp_type(s),id_connexion(i),tuile_parente(nullptr),zone_parente(nullptr), meeple_pose(nullptr) {};
 
-std::ostream& operator<<(std::ostream& f, const Case& c) {
-        f << "(Type de la zone : " << c.getZoneType() << ", Direction : " << c.getDirection() << ", Type bonus"
-            << c.getBonusType() << "," << c.getIdConnexion() << ")";
-        return f;
-    }
+std::string Case::toString() {
+    return ParametresPartie::toStringZONE_TYPE(zone_type);
+}
