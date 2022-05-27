0b3be7372f3dca357b9272be2359a175ecba9661
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-27 19:21:56 +0200

    Update Case.h

--- modele/CasesTuilesEtZones/Case.h
+++ modele/CasesTuilesEtZones/Case.h
@@ -1,58 +1,41 @@
-#ifndef Case_h
-#define Case_h
+#ifndef CARCASSONNE_LO21_P22_CASE_H
+#define CARCASSONNE_LO21_P22_CASE_H
 #pragma once
-#include <stdio.h>
-# include <iostream>
-# include <string>
-# include <initializer_list>
-# include <cstdlib>
-class Zone;
+class Meeple;
+class Case;
+#include "ParametresPartie.h"
+#include "Joueur.h"
+#include "Tuile.h"
+#include "Zone.h"
+#include "Meeple.h"
 
-
-#include "Gestion/ParametresPartie.h"
-#include "CasesTuilesetZones/Tuile.h"
-
-#include "JoueurEtRessources/Meeple.h"
-
-using namespace std;
-
-
-    class CarcassonneException {
-    public:
-        CarcassonneException(const string& i) :info(i) {}
-        string getInfo() const { return info; }
-    private:
-        string info;
-    };
-
-    
-class Case
-{
+class Case {
+    ZONE_TYPE zone_type;
+    DIRECTION direction;
+    SUPP_TYPE supp_type;
+    int id_connexion;
+    Tuile * tuile_parente;
+    Zone * zone_parente;
+    Meeple * meeple_pose;
 public:
-        ZONE_TYPE getZoneType() const ;
-        DIRECTION getDirection() const { return direction; }
-        SUPP_TYPE getBonusType() const { return supp_type;}
-        int getIdConnexion() const { return id_connexion; }
-        Tuile& getTuileParente() const { return *tuile_parente; }
-        Zone& getZoneParente() const { return *zone_parente; }
-        Meeple& getMeeplePose() const { return *meeple_posé; }
-        void setTuileParente(Tuile& t) { tuile_parente=&t;}
-        void setZoneParente(Zone& z) { zone_parente=&z;}
-        void setDirection(DIRECTION c) { direction=c; }
-        void setMeeple(Meeple& m) { meeple_posé=&m;}
-        Case(ZONE_TYPE zt, DIRECTION d,SUPP_TYPE s,int i);
-        ~Case() = default;
-        Case(const Case&) = default;
-        Case& operator=(const Case&) = default;
-private:
-        ZONE_TYPE zone_type;
-        DIRECTION direction;
-        SUPP_TYPE supp_type;
-        int id_connexion;
-        Tuile * tuile_parente;
-        Zone * zone_parente;
-        Meeple * meeple_posé;
-    };
+    ZONE_TYPE getZoneType() const  { return zone_type; }
+    DIRECTION getDirection() const { return direction; }
+    SUPP_TYPE getSuppType() const { return supp_type;}
+    int getIdConnexion() const { return id_connexion; }
+    Tuile* getTuileParente() const { return tuile_parente; }
+    Zone* getZoneParente() const { return zone_parente; }
+    Meeple* getMeeplePose() const { return meeple_pose; }
+    void setTuileParente(Tuile* t) { tuile_parente=t;}
+    void setZoneParente(Zone& z) { zone_parente=&z;}
+    void setDirection(DIRECTION c) { direction=c; }
+    void setMeeple(Meeple* m) { meeple_pose=m;}
+    void retirerMeeplePose() { meeple_pose = nullptr; }
+    Case(ZONE_TYPE zt, DIRECTION d,SUPP_TYPE s,int i);
+    ~Case() = default;
+    Case(const Case&) = default;
+    Case& operator=(const Case&) = default;
+    std::string toString();
+};
 
-#endif /* Case_hpp */
 
+#endif //CARCASSONNE_LO21_P22_CASE_H
