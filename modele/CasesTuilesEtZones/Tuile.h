532305b2bf783fd1f1160c09e200ae7f1af9f5b0
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-27 19:22:46 +0200

    Update Tuile.h

--- modele/CasesTuilesEtZones/Tuile.h
+++ modele/CasesTuilesEtZones/Tuile.h
@@ -1,16 +1,9 @@
-//
-//  Tuile.hpp
-//  Carcassonne
-//
-//  Created by Silvia on 11/05/2022.
-//
-
 #ifndef Tuile_h
 #define Tuile_h
 #pragma once
 class Tuile;
 class Case;
-#include "Gestion/ParametresPartie.h"
+#include "ParametresPartie.h"
 #include <stdio.h>
 # include <iostream>
 # include <string>
@@ -18,28 +11,22 @@ class Case;
 # include <cstdlib>
 # include <map>
 
-
-using namespace std;
-
 class Tuile {
     public:
-        map<DIRECTION,Case *> getTuile()const{return cases;};
+        std :: map<DIRECTION,Case *> getTuile()const{return cases;};
         Case* getCase(DIRECTION d)const;
-        explicit Tuile(map<DIRECTION, Case *> m, std::string cheminImage);
+        explicit Tuile(std::map<DIRECTION, Case *> cases, std::string cheminImage);
         void PivoterTuile(int nbre_rotation);
         // explicitation des opérations générées par défaut
         ~Tuile() = default;
         Tuile(const Tuile&) = default;
         Tuile& operator=(const Tuile&) = default;
+        std::string toString();
+        std::map<DIRECTION, Case *> cases;
     private:
-        map<DIRECTION,Case*> cases;
         std::string cheminImage;
         
     };
 
-
-
-
-#endif /* Tuile_h */
-
+#endif //CARCASSONNE_LO21_P22_TUILE_H
 
