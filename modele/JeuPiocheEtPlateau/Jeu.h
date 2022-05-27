4fcaedebd0e8965bb72b4339d7cf8925272328b1
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-27 19:27:49 +0200

    Update Jeu.h

--- modele/JeuPiocheEtPlateau/Jeu.h
+++ modele/JeuPiocheEtPlateau/Jeu.h
@@ -29,16 +29,48 @@ const string SUPPLEMENTS = "Supplements/";
 //  et un chemin vers les meeples paysans s'écrit : RESSOURCES_PAYSANS + MEEPLES c'est à dire : Ressources/Paysans/Meeples/
 
 class Jeu {
+    Jeu(vector<EXTENSION> extensions);
+
     static void lireDossier(const std::string &chemin, stringVec &vecteurDeStrings);
 
 public:
-    static map<DIRECTION, Case *> deepCopyMap(const map<DIRECTION, Case *>& map1);
+    static map<DIRECTION, Case *> deepCopyMap(const map<DIRECTION, Case *> &map1);
 
     static void getTuilesDesRessources(EXTENSION extension, vector<Tuile *> *tuiles);
+
     static void getMeeplesDesRessources(EXTENSION extension, vector<Meeple *> *meeples);
 
     static string getCheminFromExtension(EXTENSION extension);
 
+    const Tuile &getTuile(size_t i) const;
+
+    static void libererJeu();
+
+    size_t getNbTuiles() const { return 72; }
+
+    // désactivation de la duplication (par recopie et affectaion)
+    Jeu(const Jeu &j) = delete;
+
+    Jeu &operator=(const Jeu &j) = delete;
+
+private:
+    //static Jeu* instance;
+    struct Handler {
+        Jeu *instance = nullptr;
+
+        Handler() = default;
+
+        ~Handler() { delete instance; }
+    };
+
+    static Handler handler; // à la fin de l'execution du programme le destructeur de handler est forcément appelé
+
+    ~Jeu();
+
+    vector<EXTENSION> extension;
+    vector<Tuile *> tuilesRiviere;
+    vector<Tuile *> tuiles;
 };
+
 
 #endif //CARCASSONNE_LO21_P22_JEU_H
