a175befe7620c68c01e7bb57addc7b55b9a628f3
SilviaTr <105166324+SilviaTr@users.noreply.github.com>
2022-05-27 19:25:05 +0200

    Update Case.cpp

--- modele/CasesTuilesEtZones/Case.cpp
+++ modele/CasesTuilesEtZones/Case.cpp
@@ -5,7 +5,7 @@
 //  Created by Silvia on 11/05/2022.
 //
 
-#include "Case.h"
+#include "CasesTuilesEtZones/Case.h"
 
 
 Case::Case(ZONE_TYPE zt, DIRECTION d,SUPP_TYPE s,int i): zone_type(zt),direction(d),supp_type(s),id_connexion(i),tuile_parente(nullptr),zone_parente(nullptr), meeple_pose(nullptr) {};
