36d04592c9516ac56dd983d74a96cb9b5866048c
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-05-29 15:45:13 +0200

    tests lancement partie

--- main.cpp
+++ main.cpp
@@ -99,7 +99,96 @@ void test_ajout_user_Tuiles() {
 }
 
 int main() {
-//    test_Jeu_Pioche_Tuile();
+    int nbJoueurs;
+    vector<string> nomsJoueurs;
+    vector<EXTENSION> extensionsChoisies;
+    cout << "Hello, Carcassonne !" << endl;
+    cout << "Combien de joueurs ? ";
+    cin >> nbJoueurs;
+    for (int i = 0; i < nbJoueurs; i++) {
+        string nom;
+        cout << "nom du joueur " << i + 1 << " : ";
+        cin >> nom;
+        nomsJoueurs.push_back(nom);
+    }
+
+
+    cout << "Choisissez les extensions : " << endl;
+    cout << "1 : Abbe" << endl;
+    cout << "2 : Auberges Cathedrales" << endl;
+    cout << "3 : Marchands Batisseurs" << endl;
+    cout << "4 : Riviere" << endl;
+    cout << "5 : Tous" << endl;
+
+    int choix;
+    while (true) {
+        cout << "Entrez un numero : ";
+        cin >> choix;
+        switch (choix) {
+            case 1:
+                extensionsChoisies.push_back(EXTENSION::ABBE);
+                break;
+            case 2:
+                extensionsChoisies.push_back(EXTENSION::AUBERGES_CATHEDRALES);
+                break;
+            case 3:
+                extensionsChoisies.push_back(EXTENSION::MARCHANDS_BATISSEURS);
+                break;
+            case 4:
+                extensionsChoisies.push_back(EXTENSION::RIVIERE);
+            case 5:
+                extensionsChoisies.push_back(EXTENSION::NORMAL);
+                extensionsChoisies.push_back(EXTENSION::ABBE);
+                extensionsChoisies.push_back(EXTENSION::AUBERGES_CATHEDRALES);
+                extensionsChoisies.push_back(EXTENSION::MARCHANDS_BATISSEURS);
+                extensionsChoisies.push_back(EXTENSION::RIVIERE);
+                break;
+            default:
+                cout << "Choix invalide" << endl;
+                break;
+        }
+        if (choix != 7) {
+            cout << "Voulez-vous ajouter une extension ? " << endl;
+            cout << "1 : Oui" << endl;
+            cout << "2 : Non" << endl;
+            cin >> choix;
+            if (choix == 2) break;
+        }
+    }
+
+    extensionsChoisies.push_back(EXTENSION::NORMAL);
+    Jeu *jeu = Jeu::getInstance();
+    jeu->setExtensions(extensionsChoisies);
+    auto *pioche = new Pioche(jeu->tuiles);
+
+    Plateau *plateau = Plateau::getInstance();
+    cout << "\nDEBUT DU JEU avec ";
+    for (int i = 0; i < nbJoueurs; i++) {
+        cout << nomsJoueurs[i];
+        if (i != nbJoueurs - 1) cout << ", ";
+    }
+    cout << "\net les extensions : ";
+    cout << ParametresPartie::toStringEXTENSIONS(extensionsChoisies) << "\n" << endl;
+
+
+    while (1) {
+        for (string joueur: nomsJoueurs) {
+            Tuile *t = pioche->piocher();
+            cout << "Joueur" << joueur << " vous avez pioche la " << t->toString() << endl;
+            cout << "Position de la tuile :";
+            int x, y;
+            cin >> x >> y;
+            while (plateau->checkerTuile(t, new Coord(x, y))) {
+                cout << "Placement non valide, veuillez recommencer" << endl;
+                cout << "Position de la tuile :";
+                cin >> x >> y;
+            }
+            plateau->ajouterTuile(t, new Coord(x, y));
+            cout << "\naffichage du plateau de jeu : " << endl;
+            plateau->afficherConsole();
+            cout << "\n" << endl;
+        }
+    }
     test_ajout_user_Tuiles();
     return 0;
 }
